43a2b18946afff3900aa05dceaa4abac68fe925d
wecaners <wecaners@naver.com>
2023-12-20 10:35:00 +0900

    color value 헥사코드 -> tailwind 클래스

--- components/common/ColorChips.tsx
+++ components/common/ColorChips.tsx
@@ -15,11 +15,11 @@ interface PaletteType {
 
 function ColorChips({ onSelect }: ColorChipsProps) {
   const [isSelect, setIsSelect] = useState([
-    { key: '0', value: '#7AC555', checked: false },
-    { key: '1', value: '#760DDE', checked: false },
-    { key: '2', value: '#FFA500', checked: false },
-    { key: '3', value: '#76A5EA', checked: false },
-    { key: '4', value: '#E876EA', checked: false },
+    { key: '0', value: 'bg-green', checked: false },
+    { key: '1', value: 'bg-violet', checked: false },
+    { key: '2', value: 'bg-orange', checked: false },
+    { key: '3', value: 'bg-blue', checked: false },
+    { key: '4', value: 'bg-pink', checked: false },
   ]);
 
   const onChangeCheck =
@@ -44,8 +44,7 @@ function ColorChips({ onSelect }: ColorChipsProps) {
             className="hidden"
           />
           <motion.div
-            className="w-30pxr h-30pxr rounded-full cursor-pointer flex-center"
-            style={{ backgroundColor: element.value }}
+            className={`w-30pxr h-30pxr rounded-full cursor-pointer flex-center ${element.value}`}
             whileTap={{ scale: 0.7 }}
           >
             {element.checked && (
