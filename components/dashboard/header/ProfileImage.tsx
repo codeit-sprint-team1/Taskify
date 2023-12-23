984b018893428342297d89d829d63823748eb32e
seoyoung-min <seoyoungiyo@gmail.com>
2023-12-23 13:57:18 +0900

    feat: 프로필 이미지 컴포넌트 추가 (#52)
    * chore: react-hook-form devtool 설치
    
    * feat: 프로필이미지 컴포넌트 추가

---
+++ components/dashboard/header/ProfileImage.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+export interface ProfileImageProps {
+  name: string;
+  src: string;
+}
+
+export default function ProfileImage({ name, src }: ProfileImageProps) {
+  return (
+    <div className="rounded-full flex-center w-38pxr h-38pxr bg-green">
+      {src ? (
+        <Image src={src} alt="프로필 이미지" width="38" height="38" />
+      ) : (
+        <p className="text-white">{name[0]}</p>
+      )}
+    </div>
+  );
+}
