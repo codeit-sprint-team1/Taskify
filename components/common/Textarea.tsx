3ed58fb6529d8c87d0d6c40d8bc4c0f10a6d0bfe
이승연 <seung-yeon@iseung-yeon-ui-MacBookAir.local>
2023-12-23 18:40:51 +0900

    feat: SelectDate 컴포넌트 시간선택 기능 추가

--- components/common/Textarea.tsx
+++ components/common/Textarea.tsx
@@ -11,7 +11,11 @@ export default function Textarea({ label, required }: TextareaProps) {
         {required && <span className="text-violet">*</span>}
       </div>
       <div className="mt-2.5">
-        <textarea className="selection:block w-full rounded-md border border-solid px-16pxr py-15pxr tablet:text-16pxr mobile:text-14pxr text-gray70 placeholder:text-gray40 focus:border-violet outline-0`" />
+        <textarea
+          className="selection:block w-full rounded-md border border-solid px-16pxr py-15pxr tablet:text-16pxr mobile:text-14pxr text-gray70 placeholder:text-gray40 focus:border-violet outline-0`"
+          maxLength={200}
+          placeholder="200자 이내로 작성해주세요."
+        />
       </div>
     </div>
   );
--- components/modal/SelectDate.tsx
+++ components/modal/SelectDate.tsx
@@ -6,20 +6,29 @@ import calendarImage from '@/public/icons/calendar.svg';
 import Image from 'next/image';
 
 export default function SelectDate() {
-  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [selectDate, setSelectDate] = useState<Date | null>(null);
+  const isPossibleDay = (date: Date) => {
+    const currentDate = new Date();
+    const selectedDate = new Date(date);
+    return currentDate.getDate() <= selectedDate.getDate();
+  };
 
   return (
-    <div className=" border border-gray30 focus:border-violet">
-      <button>
-        <Image src={calendarImage} alt="달력 이미지" width={20} height={20} />
-        <DatePicker
-          dateFormat="yyyy년 MM월 dd일"
-          onChange={(date) => setStartDate(date)}
-          locale={ko}
-          className="outline-0 cursor-pointer"
-          placeholderText="날짜를 선택해주세요."
-        />
-      </button>
+    <div className=" border border-gray30 focus:border-violet flex items-center gap-10pxr w-full h-50pxr rounded-md ">
+      <Image src={calendarImage} alt="달력 이미지" width={20} height={20} />
+      <DatePicker
+        selected={selectDate}
+        dateFormat="yyyy년 MM월 dd일 HH시 mm분 "
+        onChange={(date) => setSelectDate(date)}
+        locale={ko}
+        className="outline-0 cursor-pointer w-250pxr "
+        placeholderText="날짜를 선택해주세요."
+        showTimeSelect
+        timeFormat="HH시 mm분"
+        // timeIntervals={30}
+        timeCaption="time"
+        filterDate={isPossibleDay}
+      />
     </div>
   );
 }
