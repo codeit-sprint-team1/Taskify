c245128712b71ee7a41400769920f158376d2810
Accept77 <accept_agreed@kakao.com>
2023-12-26 17:29:07 +0900

    feat: 유사 데이터 추가

--- pages/boards/index.tsx
+++ pages/boards/index.tsx
@@ -4,6 +4,117 @@ import dotIcon from '../../public/icons/boards/card-dot.svg';
 import settingIcon from '../../public/icons/boards/card-desktop-settings.svg';
 import plusIcon from '../../public/icons/boards/plus.svg';
 import Image from 'next/image';
+import { CardsList, Card } from '@/types/cards';
+import { axiosAuthInstance } from '@/utils';
+
+const mok: CardsList = {
+  cursorId: 2,
+  totalCount: 2,
+  cards: [
+    {
+      id: 1,
+      title: 'test1',
+      description: 'asdasdasdasdasdasd',
+      tags: ['dsadasd'],
+      dueDate: '2022-03-20',
+      assignee: {
+        profileImageUrl: '',
+        nickname: 'asdsadsadasdasd',
+        id: 0,
+      },
+      imageUrl: '',
+      teamId: '2',
+      columnId: 0,
+      createdAt: '2023-12-26T07:35:31.861Z',
+      updatedAt: '2023-12-26T07:35:31.861Z',
+    },
+    {
+      id: 2,
+      title: 'test2',
+      description: 'asdasdasdasdasdasd',
+      tags: ['dsadasd'],
+      dueDate: '2022-03-20',
+      assignee: {
+        profileImageUrl: '',
+        nickname: 'asdsadsadasdasd',
+        id: 0,
+      },
+      imageUrl: '',
+      teamId: '2',
+      columnId: 0,
+      createdAt: '2023-12-26T07:35:31.861Z',
+      updatedAt: '2023-12-26T07:35:31.861Z',
+    },
+    {
+      id: 3,
+      title: 'test3',
+      description: 'asdasdasdasdasdasd',
+      tags: ['dsadasd'],
+      dueDate: '2022-03-20',
+      assignee: {
+        profileImageUrl: '',
+        nickname: 'asdsadsadasdasd',
+        id: 0,
+      },
+      imageUrl: '',
+      teamId: '2',
+      columnId: 0,
+      createdAt: '2023-12-26T07:35:31.861Z',
+      updatedAt: '2023-12-26T07:35:31.861Z',
+    },
+    {
+      id: 4,
+      title: 'test4',
+      description: 'asdasdasdasdasdasd',
+      tags: ['dsadasd'],
+      dueDate: '2022-03-20',
+      assignee: {
+        profileImageUrl: '',
+        nickname: 'asdsadsadasdasd',
+        id: 0,
+      },
+      imageUrl: '',
+      teamId: '2',
+      columnId: 0,
+      createdAt: '2023-12-26T07:35:31.861Z',
+      updatedAt: '2023-12-26T07:35:31.861Z',
+    },
+    {
+      id: 5,
+      title: 'test5',
+      description: 'asdasdasdasdasdasd',
+      tags: ['dsadasd'],
+      dueDate: '2022-03-20',
+      assignee: {
+        profileImageUrl: '',
+        nickname: 'asdsadsadasdasd',
+        id: 0,
+      },
+      imageUrl: '',
+      teamId: '2',
+      columnId: 0,
+      createdAt: '2023-12-26T07:35:31.861Z',
+      updatedAt: '2023-12-26T07:35:31.861Z',
+    },
+    {
+      id: 6,
+      title: 'test6',
+      description: 'asdasdasdasdasdasd',
+      tags: ['dsadasd'],
+      dueDate: '2022-03-20',
+      assignee: {
+        profileImageUrl: '',
+        nickname: 'asdsadsadasdasd',
+        id: 0,
+      },
+      imageUrl: '',
+      teamId: '2',
+      columnId: 0,
+      createdAt: '2023-12-26T07:35:31.861Z',
+      updatedAt: '2023-12-26T07:35:31.861Z',
+    },
+  ],
+};
 
 function CardAdd() {
   return (
@@ -15,13 +126,13 @@ function CardAdd() {
   );
 }
 
-function Card() {
+function Card({ card }: { card: Card }) {
   return (
     <div className=" bg-white flex flex-col p-20pxr rounded-md gap-10pxr border-solid border border-gray30">
       <div className="bg-blue rounded-md">
         <Image src={testImg} alt="testImg" />
       </div>
-      <div className="text-gray70 font-medium">새로운 일정 관리 Taskify</div>
+      <div className="text-gray70 font-medium">{card.title}</div>
       <div>태그 논의하기</div>
       <div className="flex justify-between">
         <div className="flex-center gap-6pxr text-gray50">
@@ -55,12 +166,13 @@ function ColumnTitle() {
 
 function Column() {
   return (
-    <div className="flex flex-col w-354pxr p-20pxr bg-gray10 gap-25pxr border-solid border border-gray20">
+    <div className="flex flex-col w-354pxr h-1000pxr overflow-scroll p-20pxr bg-gray10 gap-25pxr border-solid border border-gray20">
       <ColumnTitle />
       <div className="flex-col-center gap-15pxr">
         <CardAdd />
-        <Card />
-        <Card />
+        {mok.cards.map((card) => (
+          <Card card={card} />
+        ))}
       </div>
     </div>
   );
@@ -68,10 +180,7 @@ function Column() {
 
 export default function BoardsPage() {
   return (
-    <div className="w-screen h-screen bg-pink flex-center">
-      <Column />
-      <Column />
-      <Column />
+    <div className="w-screen h-screen bg-pink flex-center gap-20pxr">
       <Column />
     </div>
   );
