b60108de823934918d4a74df6443531789b09d6f
Seoyoung <seoyoungiyo@gmail.com>
2023-12-27 18:16:32 +0900

    fix: 내 대시보드 페이지로 접속시 대시보드 목록 초기 세팅

--- pages/mydashboard/index.tsx
+++ pages/mydashboard/index.tsx
@@ -1,14 +1,23 @@
-import { Header, DashboardSidebar } from '@/components';
-import DashboardLayout from '@/page-layout/DashboardLayout';
+import { useEffect } from 'react';
 import { useDashboardList } from '@/store/memos/useDashboardList';
+import DashboardLayout from '@/page-layout/DashboardLayout';
+import { Header, DashboardSidebar } from '@/components';
+import useGetDashboardList from '@/components/dashboard/data/useGetDashboards';
 
 export default function MyDashboardPage() {
+  const { dashboards } = useGetDashboardList();
   const { dashboardList, setDashboardList } = useDashboardList();
+
+  useEffect(() => {
+    setDashboardList(dashboards);
+  }, [dashboards]);
 
   const dashboard = {
     title: '내 대시보드',
     createdByMe: false,
   };
+
+  if (!dashboardList) return;
 
   return (
     <>
