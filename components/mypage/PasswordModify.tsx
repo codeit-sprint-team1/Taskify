234b25c600621b4bda8f7bbc3bc06291fcf49456
wecaners <wecaners@naver.com>
2023-12-27 17:36:41 +0900

    feat: 반응형 디자인 추가

--- components/mypage/PasswordModify.tsx
+++ components/mypage/PasswordModify.tsx
@@ -4,7 +4,7 @@ import { Button, Input } from '..';
 function PasswordModify() {
   return (
     <div className="max-w-[620px] space-y-32pxr p-28pxr">
-      <h1 className="text-24pxr font-bold">비밀번호 변경</h1>
+      <h1 className="text-24pxr font-bold mobile:text-20pxr">비밀번호 변경</h1>
       <div className="flex flex-col gap-20pxr">
         <Input
           label="현재 비밀번호"
