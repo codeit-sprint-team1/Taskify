ee28e224d160b89c3f0c34837d2bff0dc4dade15
orrhrr <120437902+orrhrr@users.noreply.github.com>
2023-12-28 12:13:29 +0900

    feat: 담당자 드롭다운 컴포넌트 제작 (#75)
    * feat: 담당자 드롭다운 기본 레이아웃 구현
    
    * refactor: SelectDate 반응형 적용
    
    * refactor: tab키로 멤버 선택 가능하도록 수정
    
    * refactor: 드롭다운 버튼 onClick, input onBlur 함수가 순차적으로 실행되도록 수정
    
    * style: 여백 조정
    
    * remove: 불필요한 코드 삭제
    
    * remove: 불필요한 코드 삭제
    
    * refactor: 프로필이미지 size 지정하도록 수정
    
    ---------
    
    Co-authored-by: 이승연 <seung-yeon@iseung-yeon-ui-MacBookAir.local>

--- components/common/Label.tsx
+++ components/common/Label.tsx
@@ -1,12 +1,13 @@
 export interface LabelProps {
   text?: string;
   required?: boolean;
+  htmlFor?: string;
 }
 
-export default function Label({ text, required }: LabelProps) {
+export default function Label({ text, required, htmlFor }: LabelProps) {
   return (
     <div className="font-medium mb-10pxr text-18pxr text-gray70 mobile:text-16pxr ">
-      <label>{text}</label>
+      <label htmlFor={htmlFor}>{text}</label>
       {required && <span className="text-violet">*</span>}
     </div>
   );
