502526b21b8886011e58a49c7100d14f03ca7b82
Seoyoung <seoyoungiyo@gmail.com>
2023-12-28 13:44:51 +0900

    refactor: 초대하기 버튼 컴포넌트화 및 모달 연결

--- components/dashboard/header/DashboardHeader.tsx
+++ components/dashboard/header/DashboardHeader.tsx
@@ -1,17 +1,15 @@
+import { useRouter } from 'next/router';
 import Header from './Header';
+import { DashboardHeaderProps } from './Header';
 import HeaderButton from './HeaderButton';
+import InviteButton from './InviteButton';
 import addBoxIcon from '@/public/icons/add_box-icon.svg';
 import settingIcon from '@/public/icons/setting-icon.svg';
-import { DashboardHeaderProps } from './Header';
-import { useRouter } from 'next/router';
 
 export default function DashboardHeader({ dashboard }: DashboardHeaderProps) {
   const router = useRouter();
   const handleSettingButtonClick = () => {
     router.push(`${dashboard.id}/edit`);
-  };
-  const handleInvitingButtonClick = () => {
-    //초대하기 모달 띄우기
   };
   return (
     <Header dashboard={dashboard}>
@@ -24,13 +22,7 @@ export default function DashboardHeader({ dashboard }: DashboardHeaderProps) {
           >
             설정
           </HeaderButton>
-          <HeaderButton
-            onClick={handleInvitingButtonClick}
-            src={addBoxIcon}
-            alt="멤버를 초대할 수 있는 플러스 모양 아이콘"
-          >
-            초대하기
-          </HeaderButton>
+          <InviteButton />
         </div>
         <div className="desktop:w-158pxr h-34pxr w-98pxr mobile:w-82pxr bg-pink" />
         <div className="w-1pxr h-38pxr desktop:mr-32pxr mr-24pxr mobile:mr-12pxr bg-gray30" />
