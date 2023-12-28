c8124c96d80c8a395006956b38f6006706a6a0dd
wecaners <wecaners@naver.com>
2023-12-28 16:58:10 +0900

    profileImage 컴포넌트 수정

--- components/common/ProfileImage.tsx
+++ components/common/ProfileImage.tsx
@@ -7,9 +7,9 @@ export interface ProfileImageProps {
 
 export default function ProfileImage({ name, src }: ProfileImageProps) {
   return (
-    <div className="rounded-full flex-center w-38pxr h-38pxr bg-green">
+    <div className="rounded-full flex-center w-38pxr h-38pxr bg-green relative overflow-hidden">
       {src ? (
-        <Image src={src} alt="프로필 이미지" width="38" height="38" />
+        <Image src={src} alt="프로필 이미지" fill />
       ) : (
         <p className="text-white">{name[0]}</p>
       )}
