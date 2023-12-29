41632fc01a3e288301934c2942b6b805967269fd
Seoyoung <seoyoungiyo@gmail.com>
2023-12-30 00:22:28 +0900

    fix: 대시보드 사이드바 컴포넌트 persist로 인한 hydration 오류 해결

--- pages/mydashboard/index.tsx
+++ pages/mydashboard/index.tsx
@@ -1,30 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDashboardList } from '@/store/memos/useDashboardList';
 import DashboardLayout from '@/page-layout/DashboardLayout';
 import { Header, DashboardSidebar } from '@/components';
-import useGetDashboardList from '@/components/dashboard/data/useGetDashboards';
+import useGetDashboards from '@/components/dashboard/data/useGetDashboards';
+import { Dashboards } from '@/types/dashboards';
 
 export default function MyDashboardPage() {
-  const { dashboards } = useGetDashboardList();
+  const [dashboardData, setDashboardData] = useState<Dashboards[]>([]);
+  const { dashboards } = useGetDashboards();
   const { dashboardList, setDashboardList } = useDashboardList();
 
   useEffect(() => {
     setDashboardList(dashboards);
   }, [dashboards]);
 
+  useEffect(() => {
+    setDashboardData(dashboardList);
+  }, [dashboardList]);
+
   const dashboard = {
     title: '내 대시보드',
     createdByMe: false,
   };
 
-  if (!dashboardList) return;
+  if (dashboardData?.length === 0) return;
 
   return (
     <>
       <DashboardLayout
         header={<Header dashboard={dashboard} />}
-        main={<div>main</div>}
-        sidebar={<DashboardSidebar dashboardList={dashboardList} />}
+        main={'main'}
+        sidebar={<DashboardSidebar dashboardList={dashboardData} />}
       ></DashboardLayout>
     </>
   );
