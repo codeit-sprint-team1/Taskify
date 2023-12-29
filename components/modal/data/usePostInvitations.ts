20371b8e6f82514531a8053e7435f491f24d1c6f
Seoyoung <seoyoungiyo@gmail.com>
2023-12-29 16:14:33 +0900

    fix: usePostInvitations 사용 타입 변경

--- components/modal/data/usePostInvitations.ts
+++ components/modal/data/usePostInvitations.ts
@@ -1,12 +1,12 @@
 import { useCallback } from 'react';
 import { axiosAuthInstance } from '@/utils';
 import { useAsync } from '@/hooks/useAsync';
-import { Dashboards } from '@/types/dashboards';
+import { Invitations } from '@/types/invitations';
 
 const usePostInvitations = (dashboardId: string, email: string) => {
   const postDashboards = useCallback(
     () =>
-      axiosAuthInstance.post<Dashboards>(
+      axiosAuthInstance.post<Invitations>(
         `dashboards/${dashboardId}/invitations`,
         {
           email,
