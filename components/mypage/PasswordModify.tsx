785db892f015b15f138d22bc62885c88ab74e998
wecaners <wecaners@naver.com>
2023-12-29 16:17:52 +0900

    feat: 비밀번호 변경 기능 추가

--- components/mypage/PasswordModify.tsx
+++ components/mypage/PasswordModify.tsx
@@ -1,16 +1,53 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react';
-import { Button, Input, PasswordInput } from '..';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+import { Button, Modal, PasswordInput } from '..';
+import { Controller, useForm } from 'react-hook-form';
+import { ERROR_MESSAGE, VALID_PASSWORD_REG } from '../sign/constants';
+import { axiosAuthInstance } from '@/utils';
+import axios, { AxiosError } from 'axios';
+import useToggle from '@/hooks/useToggle';
+
+interface FormValues {
+  currentPassword: string;
+  newPassword: string;
+  newPasswordConfirm: string;
+}
 
 function PasswordModify() {
-  const { register, handleSubmit } = useForm({ mode: 'onBlur' });
-  const [currentPassword, setCurrentPassword] = useState<string | null>('');
-  const [newPassword, setNewPassword] = useState<string | null>('');
-  const [newPasswordConfirm, setNewPasswordConfirm] = useState<string | null>(
-    ''
-  );
+  const [axiosErrorMessage, setAxiosErrorMessage] = useState('');
+  const { handleSubmit, control, watch } = useForm<FormValues>({
+    mode: 'onBlur',
+  });
 
-  const onSubmit = () => {};
+  const { isOn, toggle } = useToggle();
+
+  const modifyPassword = async () => {
+    let res;
+    try {
+      res = await axiosAuthInstance.put(`auth/password`, {
+        password: watch('currentPassword'),
+        newPassword: watch('newPassword'),
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError;
+        if (axiosError.response) {
+          const errorMessage = (axiosError.response.data as { message: string })
+            .message;
+          setAxiosErrorMessage(errorMessage);
+          errorMessage === ERROR_MESSAGE.passwordDifferent ? toggle() : null;
+        }
+      }
+    }
+  };
+
+  const onSubmit = () => {
+    modifyPassword();
+  };
+
+  const isFulled =
+    watch('currentPassword') &&
+    watch('newPassword') &&
+    watch('newPasswordConfirm');
 
   return (
     <div className="max-w-[620px] space-y-32pxr p-28pxr">
@@ -19,38 +56,84 @@ function PasswordModify() {
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col gap-20pxr"
       >
-        <PasswordInput
-          label="현재 비밀번호"
-          value={currentPassword}
-          placeholder="현재 비밀번호 입력"
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setCurrentPassword(e.target.value)
-          }
+        <Controller
+          control={control}
+          name="currentPassword"
+          render={({ field }) => (
+            <PasswordInput
+              {...field}
+              label="현재 비밀번호"
+              placeholder="현재 비밀번호 입력"
+            />
+          )}
         />
-        <PasswordInput
-          label="새 비밀번호"
-          value={newPassword}
-          placeholder="새 비밀번호 입력"
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setNewPassword(e.target.value)
-          }
+
+        <Controller
+          control={control}
+          name="newPassword"
+          rules={{
+            pattern: {
+              value: VALID_PASSWORD_REG,
+              message: ERROR_MESSAGE.passwordInvalid,
+            },
+          }}
+          render={({ field, fieldState }) => (
+            <PasswordInput
+              {...field}
+              label="새 비밀번호"
+              placeholder="새 비밀번호 입력"
+              hasError={Boolean(fieldState.error)}
+              helperText={fieldState.error?.message}
+            />
+          )}
         />
-        <PasswordInput
-          label="새 비밀번호 확인"
-          value={newPasswordConfirm}
-          placeholder="새 비밀번호 입력"
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setNewPasswordConfirm(e.target.value)
-          }
+        <Controller
+          control={control}
+          name="newPasswordConfirm"
+          rules={{
+            validate: {
+              isMatch: (value) => {
+                if (value !== watch('newPassword')) {
+                  return ERROR_MESSAGE.confirmedPasswordNotMatch;
+                }
+                return true;
+              },
+            },
+          }}
+          render={({ field, fieldState }) => (
+            <PasswordInput
+              {...field}
+              id="newPasswordConfirm"
+              label="새 비밀번호 확인"
+              placeholder="새 비밀번호 입력"
+              hasError={Boolean(fieldState.error)}
+              helperText={fieldState.error?.message}
+            />
+          )}
         />
         <Button
-          variant="primary"
+          variant={isFulled ? 'primary' : 'inactive'}
           size="desktop"
           className="self-end mobile:w-84pxr mobile:h-28pxr"
+          disabled={!isFulled}
+          onClick={onSubmit}
         >
           변경
         </Button>
       </form>
+      <Modal isOpen={isOn} onSubmit={toggle}>
+        <div className="flex-center flex-col space-y-20pxr">
+          <p className="pt-70pxr pb-25pxr">{axiosErrorMessage}</p>
+          <Button
+            variant="primary"
+            size="modal"
+            className="self-end"
+            onClick={toggle}
+          >
+            확인
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 }
