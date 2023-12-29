bfb8005043f3e142c383f36ed4230934788acc58
Seoyoung <seoyoungiyo@gmail.com>
2023-12-30 01:27:45 +0900

    refactor: 데이터 없을 때 대비 return 문 조건 변경

--- pages/mydashboard/index.tsx
+++ pages/mydashboard/index.tsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDashboardList } from '@/store/memos/useDashboardList';
 import DashboardLayout from '@/page-layout/DashboardLayout';
 import { Header, DashboardSidebar } from '@/components';
 import useGetDashboards from '@/components/dashboard/data/useGetDashboards';
-import { Dashboards } from '@/types/dashboards';
 
 export default function MyDashboardPage() {
-  const [dashboardData, setDashboardData] = useState<Dashboards[]>([]);
   const { dashboards } = useGetDashboards();
   const { dashboardList, setDashboardList } = useDashboardList();
 
@@ -14,23 +12,19 @@ export default function MyDashboardPage() {
     setDashboardList(dashboards);
   }, [dashboards]);
 
-  useEffect(() => {
-    setDashboardData(dashboardList);
-  }, [dashboardList]);
-
   const dashboard = {
     title: '내 대시보드',
     createdByMe: false,
   };
 
-  if (dashboardData?.length === 0) return;
+  if (dashboards === undefined) return;
 
   return (
     <>
       <DashboardLayout
         header={<Header dashboard={dashboard} />}
         main={'main'}
-        sidebar={<DashboardSidebar dashboardList={dashboardData} />}
+        sidebar={<DashboardSidebar dashboardList={dashboardList} />}
       ></DashboardLayout>
     </>
   );
