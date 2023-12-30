edd64ad041021b3d7970034c02db0f130060bd2a
seoyoung-min <seoyoungiyo@gmail.com>
2023-12-30 16:57:05 +0900

    feat: 컬럼 삭제 확인 모달 추가 (#98)
    * feat: 컬럼삭제 모달 생성
    
    * refactor: 일반 함수 선언 방식 컨벤션 맞춰 변경
    
    * refactor: 불필요 코드 삭제

---
+++ components/dashboard/data/useDeleteColumns.tsx
@@ -0,0 +1,25 @@
+import { useAsync } from '@/hooks/useAsync';
+import { axiosAuthInstance } from '@/utils';
+import { useCallback } from 'react';
+
+const useDeleteColumns = (columnId: number) => {
+  const deleteColumns = useCallback(
+    () => axiosAuthInstance.delete(`columns/${columnId}`),
+    [columnId]
+  );
+
+  const { execute, loading, error, data, status } = useAsync(
+    deleteColumns,
+    true
+  );
+
+  return {
+    execute,
+    loading,
+    error,
+    data,
+    status,
+  };
+};
+
+export default useDeleteColumns;
