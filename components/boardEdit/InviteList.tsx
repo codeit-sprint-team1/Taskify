ed9ec770c3dd91c2a169a50bbb6e6e32f3df08df
wecaners <wecaners@naver.com>
2024-01-02 20:47:28 +0900

    refactor: 빈 초대 내역 아이콘 이름 변경

--- components/boardEdit/InviteList.tsx
+++ components/boardEdit/InviteList.tsx
@@ -2,7 +2,7 @@ import { Invitations } from '@/types/invitations';
 import React from 'react';
 import { Button } from '..';
 import Image from 'next/image';
-import imageTest from '@/public/icons/invitations-not-valid-icon.svg';
+import invitationEmptyIcon from '@/public/icons/invitations-not-valid-icon.svg';
 
 interface InviteListProps {
   invitations: Invitations[] | undefined;
@@ -16,7 +16,7 @@ function InviteList({ invitations, onDelete }: InviteListProps) {
     <div className="space-y-32pxr">
       {invitations?.length === 0 ? (
         <div className="flex-center flex-col">
-          <Image src={imageTest} alt="test" />
+          <Image src={invitationEmptyIcon} alt="빈 초대내역 아이콘" />
           <p className="text-gray40">
             아직 초대한 유저가 없어요! 이메일로 초대해보세요.
           </p>
