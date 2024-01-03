5cefd761b1eeac50df0966d7971a54657f4aefcb
wecaners <wecaners@naver.com>
2024-01-03 15:28:20 +0900

    feat: 뒤로가기 버튼 추가

--- components/mypage/MypageMain.tsx
+++ components/mypage/MypageMain.tsx
@@ -1,13 +1,21 @@
 import MyPageLayout from '@/page-layout/MyPageLayout';
 import React from 'react';
-import { MypageProfile, PasswordModify } from '..';
+import { ArrowBackButton, MypageProfile, PasswordModify } from '..';
+import Link from 'next/link';
 
 function MypageMain() {
   return (
-    <MyPageLayout
-      passwordModify={<PasswordModify />}
-      mypageProfile={<MypageProfile />}
-    />
+    <div>
+      <div className="w-130pxr p-20pxr">
+        <Link href={`/mydashboard`}>
+          <ArrowBackButton />
+        </Link>
+      </div>
+      <MyPageLayout
+        passwordModify={<PasswordModify />}
+        mypageProfile={<MypageProfile />}
+      />
+    </div>
   );
 }
 
