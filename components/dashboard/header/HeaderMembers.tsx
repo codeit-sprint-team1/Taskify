3018e78e2045616b57f100683e440da69402f1aa
Seoyoung <seoyoungiyo@gmail.com>
2024-01-04 14:47:34 +0900

    refactor: 헤더구성원 전체 다 보이게 스크롤 구현

--- components/dashboard/header/HeaderMembers.tsx
+++ components/dashboard/header/HeaderMembers.tsx
@@ -3,8 +3,109 @@ import useGetMembers from '@/components/boardEdit/data/useGetMembers';
 import { useEffect, useState } from 'react';
 import { Members } from '@/types/members';
 import MemberInfoItem from './MemberInfoItem';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+
+const mock = [
+  {
+    id: 1,
+    email: 'user1@example.com',
+    nickname: 'User1',
+    profileImageUrl: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isOwner: true,
+    userId: 101,
+  },
+  {
+    id: 2,
+    email: 'user2@example.com',
+    nickname: 'User2',
+    profileImageUrl: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isOwner: false,
+    userId: 102,
+  },
+  {
+    id: 3,
+    email: 'user3@example.com',
+    nickname: 'User3',
+    profileImageUrl: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isOwner: false,
+    userId: 103,
+  },
+  {
+    id: 4,
+    email: 'user4@example.com',
+    nickname: 'User4',
+    profileImageUrl: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isOwner: true,
+    userId: 104,
+  },
+  {
+    id: 5,
+    email: 'user5@example.com',
+    nickname: 'User5',
+    profileImageUrl: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isOwner: false,
+    userId: 105,
+  },
+  {
+    id: 6,
+    email: 'user6@example.com',
+    nickname: 'User6',
+    profileImageUrl: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isOwner: true,
+    userId: 106,
+  },
+  {
+    id: 7,
+    email: 'user7@example.com',
+    nickname: 'User7',
+    profileImageUrl: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isOwner: false,
+    userId: 107,
+  },
+  {
+    id: 8,
+    email: 'user8@example.com',
+    nickname: 'User8',
+    profileImageUrl: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isOwner: true,
+    userId: 108,
+  },
+  {
+    id: 9,
+    email: 'user9@example.com',
+    nickname: 'User9',
+    profileImageUrl: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isOwner: false,
+    userId: 109,
+  },
+  {
+    id: 10,
+    email: 'user10@example.com',
+    nickname: 'User10',
+    profileImageUrl: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isOwner: true,
+    userId: 110,
+  },
+];
 
 interface HeaderMembersProps {
   dashboardId: number;
@@ -45,7 +146,7 @@ export default function HeaderMembers({ dashboardId }: HeaderMembersProps) {
   } = useGetMembers({
     boardid: dashboardId,
     page: 1,
-    size: 20,
+    size: 1000,
   });
 
   useEffect(() => {
@@ -99,9 +200,8 @@ export default function HeaderMembers({ dashboardId }: HeaderMembersProps) {
             <ProfileImage textDiv name={`${restMembers?.length}+`} src="" />
           </div>
         )}
-
-        <div className="absolute right-0pxr invisible group-hover:visible p-5pxr bg-violet8 rounded-md mt-1pxr">
-          {restMembers?.map((member) => {
+        <div className="absolute flex flex-col gap-5pxr -left-15pxr max-h-200pxr overflow-scroll invisible group-hover:visible p-5pxr bg-violet8 rounded-md mt-1pxr">
+          {mock?.map((member) => {
             return (
               <MemberInfoItem
                 showImage
@@ -112,14 +212,6 @@ export default function HeaderMembers({ dashboardId }: HeaderMembersProps) {
               />
             );
           })}
-          {totalCount > 20 && (
-            <Link
-              href={`/dashboard/${dashboardId}/edit`}
-              className="block text-11pxr w-full py-5pxr text-gray50 text-center"
-            >
-              전체 멤버 보기
-            </Link>
-          )}
         </div>
       </div>
     </div>
