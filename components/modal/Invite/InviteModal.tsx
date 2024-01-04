155d866bed65de2228705cb5659a86bf8e594d95
Seoyoung <seoyoungiyo@gmail.com>
2024-01-04 14:54:15 +0900

    refactor: 멤버 초대 완료 후 notify 알림

--- components/modal/Invite/InviteModal.tsx
+++ components/modal/Invite/InviteModal.tsx
@@ -6,6 +6,7 @@ import usePostInvitations from '../data/usePostInvitations';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { Invitations } from '@/types/invitations';
+import { notify } from '@/components/common/Toast';
 
 export interface InviteModalForm {
   email: string;
@@ -58,6 +59,7 @@ export default function InviteModal({
         message: error.response.data.message,
       });
     } else {
+      notify({ type: 'success', text: '초대를 완료했습니다 💌' });
       handleCancel();
     }
   }, [error, loading]);
